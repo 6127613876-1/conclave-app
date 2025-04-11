50a9b679f86259adce51006db74f29b6cde92aa6
6127613876-1 <gokulpriyan781@gmail.com>
2025-04-12 00:06:18 +0530

    5

--- src/components/Committee.jsx
+++ src/components/Committee.jsx
@@ -2,15 +2,15 @@ import { FaLinkedin, FaXTwitter, FaInstagram, FaFacebookF } from "react-icons/fa
 
 export default function Committee() {
   const allMembers = [
-    { name: "Mr. Hari Thiagarajan", role: "Chairman & Correspondent", image: "https://www.tce.edu/sites/default/files/2023-03/Trustee.jpeg" },
-    { name: "Dr. L Ashok Kumar", role: "Principal", image: "https://www.tce.edu/sites/default/files/2024-09/PRINCIPAL.jpg" },
-    { name: "Dr. G. K. Raajesh", role: "Dean (CD)", image: "https://www.tce.edu/sites/default/files/2024-09/gk.jpeg" },
-    { name: "Mr. S. A. R. Sheik Masthan", role: "Asst. Professor, Mechatronics", image: "https://www.tce.edu/sites/default/files/2023-05/sarsmech.jpg" },
-    { name: "Mrs. C. V. Nisha Angeline", role: "Asst. Professor, IT", image: "https://www.tce.edu/sites/default/files/2023-07/cvnait.jpg" },
-    { name: "Dr. S. Ilankumaran", role: "Asst. Professor, IT", image: "https://www.tce.edu/sites/default/files/2023-07/siit.jpg" },
-    { name: "Mr. S. Arunkumar", role: "Asst. Professor, Mechanical", image: "https://www.tce.edu/sites/default/files/2023-10/sakmech.jpg" },
-    { name: "Dr. N. B. Balamurugan", role: "Assoc. Dean (CD), Professor, ECE", image: "https://www.tce.edu/sites/default/files/2023-07/nbbece.jpg" },
-    { name: "Dr. M. Vijayalakshmi", role: "Professor, CSE", image: "https://www.tce.edu/sites/default/files/2023-07/mvcse.jpg" }
+    { name: "Mr. Hari Thiagarajan", job: "Chairman & Correspondent", image: "https://www.tce.edu/sites/default/files/2023-03/Trustee.jpeg",role:"Chief Patron" },
+    { name: "Dr. L Ashok Kumar", job: "Principal", image: "https://www.tce.edu/sites/default/files/2024-09/PRINCIPAL.jpg",role:"Patron" },
+    { name: "Dr. G. K. Raajesh", job: "Dean (CD)", image: "https://www.tce.edu/sites/default/files/2024-09/gk.jpeg" ,role:"Convener" },
+    { name: "Mr. S. A. R. Sheik Masthan", job: "Asst. Professor, Mechatronics", image: "https://www.tce.edu/sites/default/files/2023-05/sarsmech.jpg",role:"Coordinator" },
+    { name: "Mrs. C. V. Nisha Angeline", job: "Asst. Professor, IT", image: "https://www.tce.edu/sites/default/files/2023-07/cvnait.jpg" ,role:"Coordinator"},
+    { name: "Dr. S. Ilankumaran", job: "Asst. Professor, IT", image: "https://www.tce.edu/sites/default/files/2023-07/siit.jpg",role:"Coordinator" },
+    { name: "Mr. S. Arunkumar", job: "Asst. Professor, Mechanical", image: "https://www.tce.edu/sites/default/files/2023-10/sakmech.jpg",role:"Coordinator" },
+    { name: "Dr. N. B. Balamurugan", job: "Assoc. Dean (CD), Professor, ECE", image: "https://www.tce.edu/sites/default/files/2023-07/nbbece.jpg",role:"Organizing Secretary" },
+    { name: "Dr. M. Vijayalakshmi", job: "Professor, CSE", image: "https://www.tce.edu/sites/default/files/2023-07/mvcse.jpg",role:"Organizing Secretary" },
   ];
 
   return (
@@ -25,13 +25,14 @@ export default function Committee() {
   );
 }
 
-function PersonCard({ name, role, image }) {
+function PersonCard({ name, job, image,role }) {
   return (
     <div className="w-80 bg-white rounded-xl shadow-lg overflow-hidden flex flex-col">
       <img src={image} alt={name} className="w-full h-80 object-cover" />
       <div className="p-4 text-center flex flex-col justify-between flex-1">
         <div>
           <h4 className="font-semibold text-gray-800 text-lg">{name}</h4>
+          <p className="text-sm text-gray-600">{job}</p>
           <p className="text-sm text-gray-600">{role}</p>
         </div>
         <div className="flex justify-center gap-5 mt-4 text-gray-500">
