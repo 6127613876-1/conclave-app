f4ecf45afd42b310f872621a0501f680228ee5dc
6127613876-1 <gokulpriyan781@gmail.com>
2025-05-10 13:19:32 +0530

    7

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,6 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
